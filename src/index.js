f8e374d468a94c4578e05ecc8b182d43f52b9a0d
jv-aquino <jvaquinocontact@gmail.com>
2022-09-20 08:10:44 -0300

    Adds basic Object and DOM setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('hi');
+const Project = (name) => {
+  const tasks = [];
+
+  const addTask = (Task) => {
+    tasks.push(Task);
+  }
+  const removeTask = (Task) => {
+    TaskIndex = tasks.indexOf(Task);
+
+    if (TaskIndex != -1) { // If the task index is valid (!= -1) ...
+      tasks.splice(TaskIndex, 1) // Removes Task
+    }
+  }
+
+  const getName = () => name;
+
+  return {addTask, getName, removeTask};
+};
+
+const Task = (name) => {
+  let checked;
+
+  const getName = () => name;
+  const isChecked = () => checked;
+
+  const toggleChecked = () => {
+    checked = !checked;
+  };
+
+  return {getName, isChecked, toggleChecked};
+};
+
+const DOM = () => {
+  const addTaskButton = document.querySelector("button.addTask");
+  const addProjectButton = document.querySelector("button.addProject");
+
+  return {};
+};
+
+const Controller = () => {
+  const createProject = () => {
+
+  };
+
+  const createTask = () => {
+
+  };
+
+  return {createProject, createTask};
+};
